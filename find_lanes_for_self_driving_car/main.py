100d71556374c83cf08dc5e55dfc1baefd85fb57
165nth <1605nth@gmail.com>
2021-02-06 20:04:41 +0700

    xong phan nhan dien

--- find_lanes_for_self_driving_car/main.py
+++ find_lanes_for_self_driving_car/main.py
@@ -1,6 +1,22 @@
 import cv2 as cv
+import numpy as np
+
+def Canny(_img):
+    gray = cv.cvtColor(_img, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(gray, (5, 5), 0)
+    canny = cv.Canny(blur, 50, 150)
+    return canny
+
+def region_of_interest(_img):
+    height = _img.shape[0]
+    triangle = np.array([[(200, height), (1100, height), (550, 250)]])
+    mask = np.zeros_like(_img)
+    cv.fillPoly(mask, triangle, 255)
+    return mask
 
 img = cv.imread("./images/test_image.jpg")
-cv.imshow("images", img)
+lane_img = np.copy(img)
+canny = Canny(lane_img)
 
+cv.imshow("images", region_of_interest(canny))
 cv.waitKey(0)
